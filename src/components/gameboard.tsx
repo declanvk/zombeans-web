b3679ddddf67022909e1778b20034c9e3adec362
Luc Bouchard <lbouchard010@gmail.com>
2019-02-01 21:40:43 -0800

    Add basic canvas implementation

--- src/components/gameboard.tsx
+++ src/components/gameboard.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+import * as io from "socket.io-client";
 
+const CANVAS_ID = 'z-gameboard-canvas-id';
 
 export
 namespace GameBoard {
@@ -12,9 +14,31 @@ namespace GameBoard {
 
 export
 class GameBoard extends React.Component<GameBoard.IProps, undefined> {
+
+  socket: SocketIO.Socket;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+
+  constructor(props: any) {
+    super(props);
+   // this.socket = io('some endpoint');
+  }
+
+  componentDidMount() {
+    // Send room code over socket
+
+    this.canvas = (document.getElementById(CANVAS_ID) as HTMLCanvasElement);
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(10, 10, 20, 20);
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <canvas id={CANVAS_ID} height={800} width={1024}
+            className={'z-gameboard-canvas'}/>
+      </div>
     );
   }
 }
