27b39b89efdfa29875f3c3376989be4615dbc982
Patrick Beninga <patrickbeninga@gmail.com>
2019-02-02 10:19:12 -0800

    maybe some rendering

--- src/components/desktop/gameboard.tsx
+++ src/components/desktop/gameboard.tsx
@@ -18,6 +18,10 @@ class GameBoard extends React.Component<GameBoard.IProps, undefined> {
   socket: SocketIO.Socket;
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
+  players:any;
+  boardSize:[number, number];
+  playerRadius:number
+
 
   constructor(props: any) {
     super(props);
@@ -32,7 +36,32 @@ class GameBoard extends React.Component<GameBoard.IProps, undefined> {
     this.ctx.fillStyle = 'red';
     this.ctx.fillRect(10, 10, 20, 20);
   }
+  draw(){
+    this.ctx.moveTo(0,0);
+    this.ctx.clearRect(0,0,this.boardSize[0], this.boardSize[1]);
+    for(var i in this.players){
+        this.drawPlayer(this.players[i]["position"], this.playerRadius, i);
+    }
 
+  }
+  makeBoard(){
+    this.ctx.moveTo(0,0);
+    this.ctx.lineTo(this.boardSize[0], 0);
+    this.ctx.lineTo(this.boardSize[0], this.boardSize[1]);
+    this.ctx.lineTo(0, this.boardSize[1]);
+    this.ctx.lineTo(0, 0);
+    this.ctx.stroke();
+  }
+  drawPlayer(pos:[number, number], radius:number, id:string){
+    this.ctx.beginPath()
+    if(this.players[id]["isZombie"] === true){
+      this.ctx.fillStyle = 'green';
+    }else{
+      this.ctx.fillStyle = 'brown';
+    }
+    this.ctx.arc(pos[0], pos[1], radius, 0, 2*Math.PI);
+    this.ctx.fill();
+  }
   render() {
     return (
       <div className={'z-game-board transition-item'}>
