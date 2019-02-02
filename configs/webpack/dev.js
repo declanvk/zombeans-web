171956d9b0bc8ed72799dd2572093f745fc20b33
Luc Bouchard <lbouchard010@gmail.com>
2019-02-02 12:37:04 -0800

    Start implementing dpad. Fix issues with dev websockets

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -6,20 +6,18 @@ const commonConfig = require('./common');
 module.exports = merge(commonConfig, {
   mode: 'development',
   entry: [
-    'react-hot-loader/patch', // activate HMR for React
     'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
-    'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
     './index.tsx' // the entry point of our app
   ],
   devServer: {
-    hot: true, // enable HMR on the server
     proxy: {
-      '/socket.io':'http://localhost:8000'
+      '/socket.io': {
+        target: 'http://localhost:8000',
+        ws: true
+      },
     }
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
   ],
 });
--- src/components/mobile/controller.tsx
+++ src/components/mobile/controller.tsx
@@ -50,10 +50,60 @@ class Controller extends React.Component<Controller.IProps, Controller.IState> {
       )
     }
     return (
-      <div className='z-mobile-controller transition-item' >
-        <p>{this.props.user_name}</p>
-        <p>{this.props.room_code}</p>
+      <div className='z-mobile-controller transition-item'
+          style={{height: window.innerHeight, width: window.innerWidth}}>
+        <div className='z-mobile-controller-player'>
+          <p>{this.props.user_name}</p>
+        </div>
+        <div className='z-mobile-controller-controller'>
+          <DirectionalPad on_press={(dir)=>{console.log(dir)}}
+              on_release={(dir)=>{console.log(dir)}}/>
+
+        </div>
       </div>
     );
   }
+}
+
+export
+namespace DirectionalPad {
+  export
+  interface IProps {
+    on_press: (button: string) => void;
+    on_release: (button: string) => void;
+  }
+}
+
+const DirectionalPad = (props: DirectionalPad.IProps) => {
+  return (
+    <div className='z-mobile-dpad'>
+      <div className='z-mobile-dpad-col'>
+        <div className='z-mobile-dpad-left z-mobile-dpad-button'
+            onMouseDown={()=>this.props.on_press('left')}
+            onMouseUp={()=>this.props.on_release('left')}>
+          
+        </div>
+      </div>
+      <div className='z-mobile-dpad-col'>
+        <div className='z-mobile-dpad-up z-mobile-dpad-button'
+            onMouseDown={()=>this.props.on_press('up')}
+            onMouseUp={()=>this.props.on_release('up')}>
+          
+        </div>
+        <div className='z-mobile-dpad-button z-mobile-dpad-spacer' />
+        <div className='z-mobile-dpad-down z-mobile-dpad-button'
+            onMouseDown={()=>this.props.on_press('down')}
+            onMouseUp={()=>this.props.on_release('down')}>
+          
+        </div>
+      </div>
+      <div className='z-mobile-dpad-col'>
+        <div className='z-mobile-dpad-right z-mobile-dpad-button'
+            onMouseDown={()=>this.props.on_press('right')}
+            onMouseUp={()=>this.props.on_release('right')}>
+          
+        </div>
+      </div>
+    </div>
+  )
 }
