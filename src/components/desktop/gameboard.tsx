d65c9cd85ffacde1c749d12b290a67b2436197ea
Declan Kelly <dkelly.home@gmail.com>
2019-02-02 21:32:14 -0800

    Refactor board rendering

--- src/components/desktop/gameboard.tsx
+++ src/components/desktop/gameboard.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as io from "socket.io-client";
-import { deflateRaw } from "zlib";
 
 const CANVAS_ID = 'z-desktop-gameboard-canvas-id';
 export
@@ -9,116 +8,184 @@ namespace GameBoard {
   export
   interface IProps {
     room_code: string;
-    gameboard_ready?: () => void;
+    window_height: number;
+    window_width: number;
+  }
+
+  export
+  interface IState {
+    board_description?: BoardDescription,
+    player_descriptions: Map<string, PlayerDescription>;
+    player_render_data: Map<string, PlayerRenderData>;
+  }
+
+  export
+  interface BoardDescription {
+    width: number;
+    height: number;
+    player_radius: number;
+  }
+
+  export
+  interface PlayerRenderData {
+    x: number;
+    y: number;
+    is_zombie: boolean;
+  }
+
+  export
+  interface PlayerDescription {
+    user_name: string;
+    character: number;
   }
 }
 
 export
-class GameBoard extends React.Component<GameBoard.IProps, undefined> {
+class GameBoard extends React.Component<GameBoard.IProps, GameBoard.IState> {
 
-  socket: SocketIO.Socket;
-  canvas: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D;
-  players = {};
-  boardSize = [0,0];
-  playerRadius:number
-  gameState = "";
-
+  private _socket: SocketIO.Socket;
+  private _canvas?: React.RefObject<HTMLCanvasElement>;
+  private _ctx?: CanvasRenderingContext2D;
 
   constructor(props: any) {
     super(props);
-    this.socket = io('/viewer');
-   // this.socket = io('some endpoint');
+    this._socket = io('/viewer');
+
+    this._onGameStarting = this._onGameStarting.bind(this);
+    this._onGameTick = this._onGameTick.bind(this);
+    this._onGameViewResponse = this._onGameViewResponse.bind(this);
+
+    this.state = {
+      board_description: null,
+      player_descriptions: new Map(),
+      player_render_data: new Map(),
+    };
+
+    this._canvas = React.createRef();
+  }
+
+  private _onGameStarting(data: any): void {
+
+  }
+
+  private _onGameTick(data: any): void {
+    let player_render_data: Map<string, GameBoard.PlayerRenderData> = data['player_pos_data'];
+
+    this.setState({
+      player_render_data
+    })
+  }
+
+  private _onGameViewResponse(data: any): void {
+    if (data['view_status'] == 'success') {
+      let player_raw_descriptions: Array<{
+        user_name: string;
+        character: number;
+        player_id: string;
+      }> = data['aux_data']['current_players'];
+      let board_description: GameBoard.BoardDescription = data['aux_data']['board_description'];
+
+      let player_descriptions = player_raw_descriptions.reduce(function(map, obj) {
+        map[obj.player_id] = {
+          user_name: obj.user_name,
+          character: obj.character
+        };
+        return map;
+      }, new Map());
+
+      this.setState({
+        board_description,
+        player_descriptions
+      });
+    } else {
+      console.warn(`Failed to request view access to room: ${this.props.room_code}`);
+    }
   }
 
   componentDidMount() {
-    // Send room code over socket
-    this.canvas = (document.getElementById(CANVAS_ID) as HTMLCanvasElement);
-    this.ctx = this.canvas.getContext('2d');
-    this.socket.on('game_starting', (data: any) => {
-      console.log(data);
-      console.log("game starting message");
-    });
-    this.socket.on('game_tick', (data: any) => {
-      console.log("here");
-      console.log(data);
-      this.update_player_pos(data["player_pos_data"])
-      console.log(this.players);
-      console.log(this.boardSize);
-      this.draw();
-    });
-    this.socket.on("game_view_response", (data:any) => {
-      console.log(data);
-      data = data["aux_data"]
-      console.log(data);
-      var cur_players = data["current_players"]
-      for(var player = 0; player < cur_players.length; player++){
-        this.players[cur_players[player]["player_id"]] = 
-        {
-          "id": cur_players[player]["player_id"],
-          "user_name": cur_players[player]["user_name"],
-          "character": cur_players[player]["character"],
-          "isZombie": false,
-          "position":{
-            "x":0,
-            "y":0
-          }
-        }
+    this._socket.on('game_starting', this._onGameStarting);
+    this._socket.on('game_tick', this._onGameTick);
+    this._socket.on('game_view_response', this._onGameViewResponse);
+
+    this._socket.emit("request_game_view", {
+      "room_code": this.props.room_code
+    })
+  }
+
+  componentDidUpdate() {
+    if (this._canvas && this.state.board_description) {
+      const canvas = this._canvas.current;
+      const ctx = canvas.getContext('2d');
+
+      let characters = new Map()
+      for (let pair of Array.from(this.state.player_descriptions)) {
+        characters[pair[0]] = pair[1].character;
       }
-      console.log(this.players)
-      var board_data = data["board_description"];
-      this.boardSize[0] = board_data["width"];
-      this.boardSize[1] = board_data["height"];
-      this.playerRadius = board_data["player_radius"];
-      this.props.gameboard_ready();
-    });
-    this.socket.emit("request_game_view", {"room_code":this.props.room_code})
-    this.draw();
 
-  }
-  update_player_pos(data:any){
-    console.log("here");
-    console.log(data);
-    for(var player in data){
-      this.players[player]["position"] = data[player]["position"];
-      this.players[player]["isZombie"] = data[player]["isZombie"];
+      draw(ctx, characters, this.state.board_description, this.state.player_render_data);
     }
+    
   }
-  draw(){
-    this.ctx.moveTo(0,0);
-    this.ctx.clearRect(0,0,this.boardSize[0], this.boardSize[1]);
-    this.makeBoard();
-    for(var i in this.players){
-        this.drawPlayer(this.players[i]["position"], this.playerRadius, i);
-    }
 
-  }
-  makeBoard(){
-    this.ctx.moveTo(10,10);
-    this.ctx.lineTo(this.boardSize[0], 10);
-    this.ctx.lineTo(this.boardSize[0], this.boardSize[1]);
-    this.ctx.lineTo(10, this.boardSize[1]);
-    this.ctx.lineTo(10, 10);
-    this.ctx.stroke();
-  }
-  drawPlayer(pos:any, radius:number, id:string){
-    console.log(id)
-    console.log(pos);
-    console.log(radius)
-    this.ctx.beginPath()
-    if(this.players[id]["isZombie"] === true){
-      this.ctx.fillStyle = 'green';
-    }else{
-      this.ctx.fillStyle = 'brown';
-    }
-    this.ctx.arc(pos.x, pos.y, radius, 0, 2*Math.PI);
-    this.ctx.stroke();
-  }
   render() {
+    let height: number;
+    let width: number;
+    if (this.state.board_description) {
+      height = this.state.board_description.height;
+      width = this.state.board_description.width;
+    } else {
+      height = 800;
+      width = 1200
+    }
+
     return (
       <div className={'z-desktop-gameboard transition-item'}>
-        <canvas id={CANVAS_ID} height={800} width={1200}/>
+        <canvas className={'z-desktop-gameboard-canvas'} id={CANVAS_ID} height={width} width={height}/>
+        <div className={'gameplay-box'}></div>
       </div>
     );
   }
+}
+
+function draw(ctx: CanvasRenderingContext2D, characters: Map<string, number>, 
+  board: GameBoard.BoardDescription, players: Map<string, GameBoard.PlayerRenderData>)
+{
+  ctx.save();
+
+  drawBoard(ctx, board.width, board.height);
+
+  let player_ids: Array<String> = Array.from(players, (v) => v[0]);
+
+  for (let p_id in player_ids) {
+    let player: GameBoard.PlayerRenderData = players[p_id];
+    let character: number = characters[p_id];
+    drawPlayer(ctx, player.x, player.y, character, player.is_zombie, board.player_radius);
+  }
+
+  ctx.restore();
+}
+
+function drawPlayer(ctx: CanvasRenderingContext2D, x: number, y: number, character, is_zombie: boolean, radius: number) {
+  ctx.save();
+
+  if (is_zombie) {
+    ctx.fillStyle = 'green';
+  } else {
+    ctx.fillStyle = 'brown';
+  }
+
+  this.ctx.arc(x, y, radius, 0, 2*Math.PI);
+  
+  ctx.restore();
+}
+
+function drawBoard(ctx: CanvasRenderingContext2D, width: number, height: number) {
+  ctx.save();
+  ctx.moveTo(0,0);
+  ctx.lineTo(width, 0);
+  ctx.lineTo(width, height);
+  ctx.lineTo(0, height);
+  ctx.lineTo(0, 0);
+  ctx.stroke();
+  ctx.restore();
 }
