b9d31e2d73203130075bd5d9c0b109645e07ef22
Luc Bouchard <lbouchard010@gmail.com>
2019-02-02 21:54:08 -0800

    Add remaining characters

--- src/data.ts
+++ src/data.ts
@@ -17,6 +17,18 @@ const pea_zombie_img = require('./assets/img/pea-zombie.png');
 const pinto_img = require('./assets/img/pinto.png');
 const pinto_zombie_img = require('./assets/img/pinto-zombie.png');
 
+const brown_img = require('./assets/img/brown.png');
+const brown_zombie_img = require('./assets/img/brown-zombie.png');
+
+const edamame_img = require('./assets/img/edamame.png');
+const edamame_zombie_img = require('./assets/img/edamame-zombie.png');
+
+const red_img = require('./assets/img/red.png');
+const red_zombie_img = require('./assets/img/red-zombie.png');
+
+const white_img = require('./assets/img/white.png');
+const white_zombie_img = require('./assets/img/white-zombie.png');
+
 export
 const characters = [
   {normal_img: black_img,    zombie_img: black_zombie_img},
@@ -25,8 +37,8 @@ const characters = [
   {normal_img: kidney_img,   zombie_img: kidney_zombie_img},
   {normal_img: pea_img,      zombie_img: pea_zombie_img},
   {normal_img: pinto_img,    zombie_img: pinto_zombie_img},
-  {normal_img: black_img,    zombie_img: black_zombie_img},
-  {normal_img: black_img,    zombie_img: black_zombie_img},
-  {normal_img: black_img,    zombie_img: black_zombie_img},
-  {normal_img: black_img,    zombie_img: black_zombie_img},
+  {normal_img: brown_img,    zombie_img: brown_zombie_img},
+  {normal_img: edamame_img,    zombie_img: edamame_zombie_img},
+  {normal_img: red_img,    zombie_img: red_zombie_img},
+  {normal_img: white_img,    zombie_img: white_zombie_img},
 ]
